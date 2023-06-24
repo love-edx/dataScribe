055408e4abc073a460668d02fa47dc4055934ec8
love-edx <l.tyagi@edexa.team>
2023-06-25 00:32:46 +0530

    chore: added db sync cmd

--- src/common/models/index.ts
+++ src/common/models/index.ts
@@ -2,6 +2,6 @@ import { dbConfig } from '../loaders/database';
 import { UsersFactory } from './users.model';
 
 // syncing
-// dbConfig.sync({}).then(() => { console.log(`Database & tables created!`) });
+dbConfig.sync({}).then(() => { console.log(`Database & tables created!`) });
 
 export const Users = UsersFactory(dbConfig);
