ca47a505cc29829f96ce4a723c6d8621732b0174
love-edx <l.tyagi@edexa.team>
2023-06-24 23:41:05 +0530

    feat: login, signup and get profile api

--- src/api/interface/IUsers.ts
+++ src/api/interface/IUsers.ts
@@ -1,9 +1,42 @@
 import { Op } from 'sequelize';
+import * as l10n from 'jm-ez-l10n';
+import { v4 as uuidv4 } from 'uuid';
 import statusCode from '../../common/utils/StatusCodes';
+import { Users } from "../../common/models";
+import { MODULE_NAME, REQUEST_METHOD } from "../../common/utils/Constant";
+import { genHashToken } from "../../common/services/Helper";
+import { generateJWTToken } from "../../common/services/jwtTokens";
+import Container from "typedi";
 
 export class IUser {
     static async signUp(data: any) {
         try {
+            let admin = await Users.findOne({ where: { email: data['email'].toLowerCase() }, raw: true });
+            if (admin) return { status: statusCode.ALREADY_EXIST, message: `${MODULE_NAME.EMAIL} already exists` };
+
+            const invitationToken = genHashToken(50);
+
+            const dataToSave: any = {
+                firstName: data.firstName ? data.firstName : data.email.split('@')[0],
+                lastName: data.lastName,
+                password: data.password,
+                email: data.email.toLowerCase(),
+                userId: uuidv4(),
+                isActive: '0', // will be in-active until password is not set
+                invitationToken: invitationToken,
+                token: null
+            }
+            await Users.create(dataToSave);
+
+            // Send set password email
+            /* await serviceMailer.setPasswordMail({
+                sentFrom: 'DataScribe',
+                receiver_email: data.email.toLowerCase(),
+                invitationToken: invitationToken,
+                name: data['email'].toLowerCase().split('@')[0].replace(/[^a-zA-Z0-9]/g, '')
+            }); */
+            return { status: statusCode.OK, message: l10n.t('COMMON_SUCCESS', { key: MODULE_NAME.USER, method: REQUEST_METHOD.REGISTERED }) };
+
 
         } catch (error) {
             console.error(error);
@@ -13,16 +46,16 @@ export class IUser {
 
     static async signIn(data: any) {
         try {
+            const attributes = ['id', 'email', 'userId', 'firstName', 'gender', 'isActive', 'createdAt'];
+            let user = await Users.findOne({ where: { email: data.email, password: data.password }, attributes });
+            if (!user) return { status: statusCode.NOTFOUND, message: l10n.t('NOT_EXISTS', { key: MODULE_NAME.USER }) };
 
-        } catch (error) {
-            console.error(error);
-            return { status: statusCode.INTERNAL_SERVER_ERROR, message: 'SOMETHING_WENT_WRONG' };
-        }
-    }
+            let tokenData = { id: user.id, uid: user.userId, email: user.email };
+            const jwtToken = await generateJWTToken(tokenData);
 
-    static async forgetPassword(data: any) {
-        try {
+            await Users.update({ token: jwtToken, isActive: true }, { where: { userId: user.userId, id: user.id } });
 
+            return { status: statusCode.OK, message: l10n.t('COMMON_SUCCESS', { key: `${MODULE_NAME.USER} details`, method: REQUEST_METHOD.PUT }) };
         } catch (error) {
             console.error(error);
             return { status: statusCode.INTERNAL_SERVER_ERROR, message: 'SOMETHING_WENT_WRONG' };
@@ -31,7 +64,31 @@ export class IUser {
 
     static async verifyPassword(data: any) {
         try {
+            return { status: statusCode.OK, message: `${MODULE_NAME.USER} ${MODULE_NAME.EMAIL} verified successfully` };
+        } catch (error) {
+            console.error(error);
+            return { status: statusCode.INTERNAL_SERVER_ERROR, message: 'SOMETHING_WENT_WRONG' };
+        }
+    }
 
+    static async getProfile(data: any) {
+        try {
+            const tokenData: any = Container.get('auth-token');
+
+            const attributes = ['email', 'userId', 'firstName', 'gender', 'isActive', 'createdAt'];
+            const user = await Users.findOne({ where: { userId: tokenData.uid }, attributes });
+            if (!user) return { status: statusCode.NOTFOUND, message: l10n.t('NOT_EXISTS', { key: MODULE_NAME.USER }) };
+
+            return { status: statusCode.OK, message: `${MODULE_NAME.USER} details ${REQUEST_METHOD.GET} successfully`, data: user };
+        } catch (error) {
+            console.error(error);
+            return { status: statusCode.INTERNAL_SERVER_ERROR, message: 'SOMETHING_WENT_WRONG' };
+        }
+    }
+
+    static async forgetPassword(data: any) {
+        try {
+            return { status: 200, message: `${MODULE_NAME.EMAIL} sent successfully` };
         } catch (error) {
             console.error(error);
             return { status: statusCode.INTERNAL_SERVER_ERROR, message: 'SOMETHING_WENT_WRONG' };
--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,29 +1,19 @@
 import { Router, Request, Response } from 'express';
 import statusCode from '../../common/utils/StatusCodes';
 import { IUser } from '../interface/IUsers';
+import { isAuth } from "../middlewares/Authorizationn";
 
 const route = Router();
 
 export default (app: Router) => {
-    app.use('/user', route);
+    app.use('', route);
 
-    route.post('/signin', /* isAuth */ signIn);
-    route.post('/signup', /* isAuth */ signUp);
-    route.post('/verify', /* isAuth */ verifyPassword);
-    route.post('/forget-password', /* isAuth */ forgetPassword);
+    route.post('/signup', signUp);
+    route.post('/signin', signIn);
+    route.post('/verify', isAuth, verifyPassword);
+    route.get('/profile', isAuth, getProfile);
+    route.post('/forget-password', isAuth, forgetPassword);
 };
-
-async function signIn(req: Request, res: Response) {
-    const data = req.body;
-    IUser.signIn(data)
-        .then(response => {
-            res.status(response.status).json(response);
-        })
-        .catch(e => {
-            console.error(e);
-            return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ status: statusCode.INTERNAL_SERVER_ERROR, message: 'something went wrong' });
-        });
-}
 
 async function signUp(req: Request, res: Response) {
     const data = req.body;
@@ -37,9 +27,34 @@ async function signUp(req: Request, res: Response) {
         });
 }
 
+async function signIn(req: Request, res: Response) {
+    const data = req.body;
+    IUser.signIn(data)
+        .then(response => {
+            res.status(response.status).json(response);
+        })
+        .catch(e => {
+            console.error(e);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ status: statusCode.INTERNAL_SERVER_ERROR, message: 'something went wrong' });
+        });
+}
+
 async function forgetPassword(req: Request, res: Response) {
     const data = req.body;
     IUser.forgetPassword(data)
+        .then(response => {
+            res.status(response.status).json(response);
+        })
+        .catch(e => {
+            console.error(e);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).json({ status: statusCode.INTERNAL_SERVER_ERROR, message: 'something went wrong' });
+        });
+}
+
+async function getProfile(req: Request, res: Response) {
+    const data = req.headers;
+    console.log({ data })
+    IUser.getProfile(data)
         .then(response => {
             res.status(response.status).json(response);
         })
